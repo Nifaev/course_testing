7f91f3b7259e616cab934ff183e1ffb3a3fe6a3b
nifaev <nifaev.ivan@gmail.com>
2024-09-03 12:54:18 +0300

    Создан проект, написан метод решения квадратного уравнения, написан план тестирования

---
+++ task1/quadratic_equation.py
@@ -0,0 +1,20 @@
+from math import sqrt
+
+
+def solve_quadratic_equation(a, b, c):
+    if a == 0:
+        return ValueError
+
+    d = b * b - 4 * a * c
+
+    if d < 0:
+        return None
+    elif d == 0:
+        return -b / (2 * a)
+    else:
+        x1 = (-b + sqrt(d)) / (2 * a)
+        x2 = (-b - sqrt(d)) / (2 * a)
+        return min(x1, x2), max(x1, x2)
+
+
+print(solve_quadratic_equation(1, -5, 4))
